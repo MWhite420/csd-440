165027f1e6e7ed162bacdb22b29c667cd4658710
Mwhite420 <marwhite4@my365.bellevue.edu>
2025-08-31 17:39:14 -0700

    update

--- module-4/MarkPalindrome.php
+++ module-4/MarkPalindrome.php
@@ -1,3 +1,5 @@
+
+
 <?php
 /*
 Mark White
@@ -5,25 +7,34 @@ CSD440
 Mod4
 */
 
-require 'palindromeTester.php';
+// Function to test if a string is a palindrome
+function isPalindrome($string) {
+    // Normalize: lowercase and remove spaces
+    $normalized = strtolower(str_replace(' ', '', $string));
+    // Reverse string
+    $reversed = strrev($normalized);
+    // Compare
+    return $normalized === $reversed;
+}
 
 // Test strings (3 palindromes, 3 not)
 $testString= [
     "level",        // palindrome
     "Racecar",      // palindrome (case insensitive)
-    "Stanley Yelnats", // palindrome (removes spaces/case) From book/movie holes
+    "Stanley Yelnats", // palindrome (removes spaces/case) From book/movies
     "hello",        // not
     "world",        // not
     "PHP coding"    // not
 ];
 
+// Run tests
 foreach ($testString as $text) {
     $normalized = strtolower(str_replace(' ', '', $text));
     $reversed = strrev($normalized);
     $result = isPalindrome($text) ? "Palindrome" : "Not a palindrome";
 
     echo "Original: $text<br>";
-    echo "Reversed: " . strrev($text) . "<br>";
+    echo "Reversed: $reversed <br>";
     echo "Result: $result<br><br>";
 }
 ?>
